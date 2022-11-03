3ea5edb7ca8bf461a2ea8c44a997412ca88158e6
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-11-03 13:45:49 +0200

    Add header buttons for changing tab
    No tabs yet and buttons don't do anything.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-import init from "./modules/init";
+import Home from './modules/home';
+import Contact from './modules/contact';
+import Menu from './modules/menu'
 
-init();
+import './styles/main.css'
+
+function switchTab(event) {
+    console.log(event.target.dataset.page)
+}
+
+const HeaderButton = target => {
+    let button = document.createElement('button');
+    button.textContent = target
+    button.dataset.page = target
+    button.addEventListener('click', switchTab);
+    button.classList.add('header-button');
+    return button;
+}
+
+const Header = () => {
+    let header = document.createElement('header');
+    
+    const neededButtons = ['home', 'menu', 'contact'];
+    const headerButtons = neededButtons.map(target => HeaderButton(target));
+
+    header.replaceChildren(...headerButtons);
+
+    return header;
+}
+
+let body = document.querySelector('body');
+body.insertBefore(Header(), body.firstChild);
+
+
+
+
---
+++ src/modules/contact.js
@@ -0,0 +1,7 @@
+export default function() {
+    let div = document.createElement("div");
+    let header = document.createElement("h2");
+    header.textContent = "Contact us";
+    div.appendChild(header);
+    return div;
+}
