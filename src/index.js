33f4b7e7458e756241baa3e35f8ac6c1de17f79b
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-11-03 12:54:40 +0200

    Add some stuff to homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+import CupcakeImage from "./images/cupcakes.jpg"
+
+let content = document.querySelector("#content");
+
+let header = document.createElement('h1');
+let img = document.createElement('img');
+let about = document.createElement('p');
+
+header.textContent = "Nice Restaruant"
+img.src = CupcakeImage
+img.width = 400;
+about.textContent = "This restaurant has got the best cupcake ever!";
+
+content.appendChild(header);
+content.appendChild(img);
+content.appendChild(about);
